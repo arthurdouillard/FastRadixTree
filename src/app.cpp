aac8c365e0167c9c653a95bf3418f08abe3cf002
Arthur Douillard <douill_a@epita.fr>
2017-07-19 20:04:19 +0200

    Switch behavior between app and compiler.

--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-#include "utils/utils.hh"
 
 int main(int argc, char *argv[]) {
-    std::string path(argv[1]);
-    create_trie(path);
+    std::cout << "App" << std::endl;
 }
--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "utils/utils.hh"
+
 int main(int argc, char *argv[]) {
-    std::cout << "Compiler" << std::endl;
+    std::string path(argv[1]);
+    //create_trie(path);
 }
